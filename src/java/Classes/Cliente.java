e9d33bea9bf4a4c724a4c5164c39855e1a74f0ae
Artur <arturfulgonixavier@gmail.com>
2017-09-24 16:41:35 -0300

    Criação da classe Cliente e seus respectivos getters e setters.

---
+++ src/java/Classes/Cliente.java
@@ -0,0 +1,58 @@
+package Classes;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String rg;
+    private String endereço;
+    private String telefone;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
