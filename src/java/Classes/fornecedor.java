353336beb1bf474476d9e646281fccefdc047c47
Gabriela Duarte <gabrielasantosduarte@yahoo.com.br>
2017-09-24 19:14:57 -0300

    add_classe_fornecedor

---
+++ src/java/Classes/fornecedor.java
@@ -0,0 +1,61 @@
+package Classes;
+
+public class fornecedor {
+    private String nome;
+    private String razao;
+    private String cnpj;
+    private String endereço;
+    private String telefone;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazao() {
+        return razao;
+    }
+
+    public void setRazao(String razao) {
+        this.razao = razao;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
