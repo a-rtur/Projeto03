f6ad8ada63e789fcd6dc9a5a171ceae72fa20d40
Artur <arturfulgonixavier@gmail.com>
2017-09-26 13:22:46 -0300

    Classe Banco: Adicionado getter e setter para armazenamento de uma variável.

--- src/java/Classes/Banco.java
+++ src/java/Classes/Banco.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class Banco {
     private static ArrayList<Cliente> clientes;
+    private static ArrayList<Fornecedor> fornecedores;
+    private static int index;
+    
     public static ArrayList<Cliente> getCliente(){
         if(clientes==null){
             clientes = new ArrayList<>();
@@ -11,11 +14,21 @@ public class Banco {
         return clientes;
     }
     
-    private static ArrayList<Fornecedor> fornecedores;
     public static ArrayList<Fornecedor> getFonecedor(){
         if (fornecedores==null){
             fornecedores = new ArrayList<>();
         }
         return fornecedores;
     }
+
+    public static int getIndex() {
+        return index;
+    }
+
+    public static void setIndex(int valor) {
+        index = valor;
+    }
+    
+
+    
 }
