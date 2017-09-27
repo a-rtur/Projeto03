b49d817dedb005c0aff52f958dec99459b36c70f
Artur <arturfulgonixavier@gmail.com>
2017-09-27 02:40:09 -0300

    Corrigido sintaxe e erros na classe Fornecedor.

--- src/java/Classes/Fornecedor.java
+++ src/java/Classes/Fornecedor.java
@@ -1,27 +1,112 @@
 package Classes;
 
 public class Fornecedor {
-    private String nome;
     private String razao;
+    private String nomef;
     private String cnpj;
     private String endereco;
     private String telefone;
     private String email;
-
-    public String getNome() {
-        return nome;
+    
+    public static boolean verificarCnpj(String cnpj){
+        boolean retorno = false;
+        for(int i = 0; i <Banco.getFornecedor().size(); i++){
+            if(Banco.getFornecedor().get(i).getCnpj().equals(cnpj)){
+                    i = Banco.getFornecedor().size() + 1;
+                    retorno = true;
+            }
+            else {
+                retorno = false;
+            }
+        }
+        return retorno;
     }
-
-    public void setNome(String nome) {
-        this.nome = nome;
+    
+      public static void cadastrarFornecedor(String razao, String nome, String cnpj, String email, String endereco, String telefone){
+          Fornecedor f = new Fornecedor();
+          f.setRazao(razao);
+          f.setNome(nome);
+          f.setCnpj(cnpj);
+          f.setEmail(email);
+          f.setTelefone(telefone);
+          f.setEndereco(endereco);
+          Banco.getFornecedor().add(f);
+     }
+    
+    public static int pesquisarBanco(int valor, String pesquisa){
+        boolean podeEntrar = true;
+        int retorno = -1;
+        if (podeEntrar == true) {
+            String separadorTexto [] = Banco.getFornecedor().get(valor).getNome().split(" ");
+                for (int i = 0; i < separadorTexto.length; i++) {
+                if (separadorTexto[i].equals(pesquisa)) {
+                    retorno = valor;
+                    podeEntrar = false;
+                }
+            }
+        }
+        if (podeEntrar == true) {
+            String separadorTexto [] = Banco.getFornecedor().get(valor).getRazao().split(" ");
+                for (int i = 0; i < separadorTexto.length; i++) {
+                if (separadorTexto[i].equals(pesquisa)) {
+                    retorno = valor;
+                    podeEntrar = false;
+                }
+            }
+        }
+        if (podeEntrar == true) {
+            String separadorTexto [] = Banco.getFornecedor().get(valor).getCnpj().split(" ");
+                for (int i = 0; i < separadorTexto.length; i++) {
+                if (separadorTexto[i].equals(pesquisa)) {
+                    retorno = valor;
+                    podeEntrar = false;
+                }
+            }
+        }
+        if (podeEntrar == true) {
+            String separadorTexto [] = Banco.getFornecedor().get(valor).getEmail().split(" ");
+                for (int i = 0; i < separadorTexto.length; i++) {
+                if (separadorTexto[i].equals(pesquisa)) {
+                    retorno = valor;
+                    podeEntrar = false;
+                }
+            }
+        }
+        if (podeEntrar == true) {
+            String separadorTexto [] = Banco.getFornecedor().get(valor).getTelefone().split(" ");
+            for (int i = 0; i < separadorTexto.length; i++) {
+                if (separadorTexto[i].equals(pesquisa)) {
+                    retorno = valor;
+                    podeEntrar = false;
+                }
+            }
+        }
+        if (podeEntrar == true) {
+            String separadorTexto [] = Banco.getFornecedor().get(valor).getEndereco().split(" ");
+            for (int i = 0; i < separadorTexto.length; i++) {
+                if (separadorTexto[i].equals(pesquisa)) {
+                    retorno = valor;
+                    podeEntrar = false;
+                }
+            }
+        }
+        return retorno;
     }
-
+    
     public String getRazao() {
         return razao;
     }
 
     public void setRazao(String razao) {
         this.razao = razao;
+    }
+
+    public String getNome() {
+        return nomef;
+    }
+
+    public void setNome(String nomef) {
+        this.nomef = nomef;
     }
 
     public String getCnpj() {
@@ -55,7 +140,5 @@ public class Fornecedor {
     public void setEmail(String email) {
         this.email = email;
     }
-    
-    
-    
+
 }
